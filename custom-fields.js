9ea3749b9cee5031c647e0194274a8ae951c7c2c
Stapleru <96135823+Stapleru@users.noreply.github.com>
2023-11-23 01:11:05 +0300

    first commit

---
+++ custom-fields.js
@@ -0,0 +1,18 @@
+module.exports = {
+    firstname: '5ff7b15c6e67519ae4de0f8f2c13f3842554774d',
+    lastname: 'c43b09bb06a2aa90abea6e4e13ae95e4fbdc1c2b',
+    phone: 'de8d0703f8553a5d3f5635038363a8eac536c20b',
+    email: '7d5d8353d8267862a3acf41c9d88b6977c9c28b0',
+    jobdesc: '16df611494f85f20551fe75f4726eeee8082ad2e',
+    jobsrc: 'c102771097e6243027a9a8be2a7991473c23597c',
+    jobtype: '6e06e89fa30ee1a3e95f72a10af3422c310712e9',
+    address: '14557cb4b3231f39ae1367621f967f50ae8805d0',
+    city: '6abb83b1135152c442cf6610e4204b98bec46f45',
+    state: '43d82676ab5dc7fab88c88a674b50fbfe16c025b',
+    zip: '829b05788eac94d30984200502c51d3b00cdd4d0',
+    area: '53f28a76d1f7593ab761a81b09d1f1eb63325cb1',
+    startDate: '9eab5617d866896e5bbd6c85055a49d80c8cf8f2',
+    startTime: '2ac14356adee6bffe86293cc764dc3ea186f5757',
+    endTime: '426dabeacfb10f0618725fb88f54516ec9ffb5cd',
+    testSelect: '16136605dab77d2cd38e22feddc1d19977f1dbf6'
+} 
---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const path = require('path');
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+
+const api = require('./api');
+const config = require('./config');
+const User = require('./db/user');
+const customField = require('./custom-fields');
+const bodyParser = require('body-parser');
+
+User.createTable();
+
+const app = express();
+const port = 3000;
+
+passport.use(
+	'pipedrive',
+	new OAuth2Strategy({
+			authorizationURL: 'https://oauth.pipedrive.com/oauth/authorize',
+			tokenURL: 'https://oauth.pipedrive.com/oauth/token',
+			clientID: config.clientID || '',
+			clientSecret: config.clientSecret || '',
+			callbackURL: config.callbackURL || ''
+		}, async (accessToken, refreshToken, profile, done) => {
+			const userInfo = await api.getUser(accessToken);
+			const user = await User.add(
+				userInfo.data.name,
+				accessToken,
+				refreshToken
+			);
+
+			done(null, { user });
+		}
+	)
+);
+
+app.disable('x-powered-by');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(async (req, res, next) => {
+	req.user = await User.getById(1);
+	next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/auth/pipedrive', passport.authenticate('pipedrive'));
+app.get('/auth/pipedrive/callback', passport.authenticate('pipedrive', {
+	session: false,
+	failureRedirect: '/',
+	successRedirect: '/'
+}));
+app.get('/', async (req, res) => {
+	if (req.user.length < 1) {
+		return res.redirect('/auth/pipedrive');
+	}
+
+	try {
+		res.render('form');
+	} catch (error) {
+		console.log(error);
+
+		return res.send('Failed to get deals');
+	}
+});
+
+app.post('/deal', async (req, res) => {
+	try {
+		const newDeal = await api.addDeal(req.user[0].access_token, {title: `JOB #${Math.floor(Math.random() * (999999 + 1))}`, ...req.body});
+		console.log(newDeal.data.data.id);
+		return res.render('success', { id: newDeal.data.data.id });
+	} catch (error) {
+		console.log(error)
+		return res.send("Couldn't create new deal");
+	}
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
